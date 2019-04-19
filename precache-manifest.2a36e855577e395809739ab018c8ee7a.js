06fdde55a7ed47faf24ef0352191ea1ed9af3ffc
David Valachovic <David.E.Valachovic@gmail.com>
2019-04-19 10:45:29 -0500

    Updates

---
+++ precache-manifest.2a36e855577e395809739ab018c8ee7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rp-front-end/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "941577c54bf2faca4429",
+    "url": "/rp-front-end/static/js/runtime~main.4669710f.js"
+  },
+  {
+    "revision": "315ed7fcbceb56e6d9ba",
+    "url": "/rp-front-end/static/js/main.76b0919d.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/rp-front-end/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "315ed7fcbceb56e6d9ba",
+    "url": "/rp-front-end/static/css/main.d3712e3d.chunk.css"
+  },
+  {
+    "revision": "0a7c2fbb58c88437d343dfdb26f543cb",
+    "url": "/rp-front-end/index.html"
+  }
+];
